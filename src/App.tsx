1f18a15c8dd524fe3cad12ff27cc95e22ba44d79
Fernando Baroni <f@baroni.tech>
2021-02-21 01:04:39 -0300

    Creating Dashboard view

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
 import React from "react";
 import "./App.css";
 import "antd/dist/antd.css";
-import { Button } from "antd";
 import MoneeeyStore, { MoneeeyStoreProvider } from "./MoneeeyStore";
-import {
-  SampleAccounts,
-  SampleCurrencies,
-  SampleTransactions,
-} from "./Samples";
 import { TagsHighlightProvider } from "./Tags";
 import { NavigationArea } from "./Navigation";
 import { Observe } from "./Observable";
 import AppMenu from "./AppMenu";
 import AccountAndTagTransactions from "./AccountAndTagTransactions";
+import Dashboard from "./Dashboard";
 
 function App(): React.ReactElement {
   const [moneeeyStore] = React.useState(new MoneeeyStore());
 
-  const addSamples = () => {
-    if (moneeeyStore.currencies.all().length === 0) {
-      SampleCurrencies.forEach((c) => moneeeyStore.currencies.add(c));
-    }
-    if (moneeeyStore.accounts.all().length === 0) {
-      SampleAccounts.forEach((a) => moneeeyStore.accounts.add(a));
-    }
-    SampleTransactions.forEach((t) => moneeeyStore.transactions.add(t));
-  };
-
   const renderAreaContent = (area: NavigationArea) => {
     switch (area) {
       case NavigationArea.Dashboard:
-        return <span>Choose an account in Transactions</span>;
+        return <Dashboard />;
       case NavigationArea.Reports:
         return <span>Something amazing will be here</span>;
       case NavigationArea.Payees:
@@ -42,7 +27,6 @@ function App(): React.ReactElement {
         return (
           <>
             <AccountAndTagTransactions moneeeyStore={moneeeyStore} />
-            <Button onClick={addSamples}>Add Samples</Button>
           </>
         );
     }
---
+++ src/Dashboard.tsx
@@ -0,0 +1,38 @@
+import { Button } from "antd";
+import React from "react";
+import { compareDates } from "./Entity";
+import { useMoneeeyStore } from "./MoneeeyStore";
+import {
+  SampleCurrencies,
+  SampleAccounts,
+  SampleTransactions,
+} from "./Samples";
+import TransactionTable from "./TransactionTable";
+
+export default function Dashboard() {
+  const moneeeyStore = useMoneeeyStore();
+
+  const addSamples = () => {
+    if (moneeeyStore.currencies.all().length === 0) {
+      SampleCurrencies.forEach((c) => moneeeyStore.currencies.add(c));
+    }
+    if (moneeeyStore.accounts.all().length === 0) {
+      SampleAccounts.forEach((a) => moneeeyStore.accounts.add(a));
+    }
+    SampleTransactions.forEach((t) => moneeeyStore.transactions.add(t));
+  };
+
+  return (
+    <div>
+      <b>Recent Transactions</b>
+      <TransactionTable
+        moneeeyStore={moneeeyStore}
+        transactions={moneeeyStore.transactions
+          .all()
+          .sort((a, b) => compareDates(a.updated || "", b.updated || ""))
+          .splice(0, 5)}
+      />
+      <Button onClick={addSamples}>Add Samples</Button>
+    </div>
+  );
+}
