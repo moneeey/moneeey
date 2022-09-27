d66f29bed963c19536bd96564689f6bc1b0da12c
Fernando Baroni <f@baroni.tech>
2022-09-27 01:14:36 -0300

    feat: eslint cypress

--- frontend/cypress/e2e/tour.cy.ts
+++ frontend/cypress/e2e/tour.cy.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line spaced-comment
 /// <reference types="cypress" />
 
 import loc from '../support/locators'
@@ -12,7 +13,9 @@ describe('Tour spec', () => {
   it('Start tour', () => {
     cy.contains('Welcome to Moneeey')
     cy.contains('Start Tour').click()
-    cy.contains('The first step to archive your financial freedom is to let us know what currencies are you working with')
+    cy.contains(
+      'The first step to archive your financial freedom is to let us know what currencies are you working with'
+    )
     cy.wait(100)
     cy.get(loc.TOUR.NEXT_BUTTON).click()
     cy.contains('Now that we know the currencies we are having, it is time to tell us what are your accounts')
