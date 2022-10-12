381ec537a130dd5da0ed5641ac9c8b17bc0d68c0
Fernando Baroni <f@baroni.tech>
2022-10-12 11:36:16 -0300

    fix: cypress on CI

--- frontend/cypress/e2e/tour.cy.ts
+++ frontend/cypress/e2e/tour.cy.ts
@@ -4,12 +4,10 @@
 import loc from '../support/locators';
 import '../support/cypress-indexeddb-namespace';
 
-const getAppEndpoint = () => 'http://local.moneeey.io:' + (Cypress.env('FRONTEND_PORT') || 80);
-
 describe('Tour spec', () => {
   before(() => {
     cy.clearIndexedDb('_pouch_moneeey');
-    cy.visit(getAppEndpoint());
+    cy.visit('/');
   });
 
   it('Start tour', () => {
@@ -18,16 +16,14 @@ describe('Tour spec', () => {
     cy.contains(
       'The first step to archive your financial freedom is to let us know what currencies are you working with'
     );
-    cy.wait(100);
-    cy.get(loc.TOUR.NEXT_BUTTON).click();
+    cy.get(loc.TOUR.NEXT_BUTTON).should('be.visible').click();
     cy.contains('Now that we know the currencies we are having, it is time to tell us what are your accounts');
     cy.get(loc.ACCOUNTS.NAME_INPUT).type('Account test');
-    cy.wait(100);
-    cy.get(loc.TOUR.NEXT_BUTTON).click();
+    cy.get(loc.TOUR.NEXT_BUTTON).should('be.visible').click();
     cy.contains('New budget').click();
     cy.get(loc.BUDGET.NAME_INPUT).type('Budget test');
     cy.get('[data-test-id="budgetCurrency"]').click(); // TODO: fix me, this is so dirty
-    cy.get('[title="Real brasileiro"]').click();
+    cy.get('[title="Real brasileiro"]').should('be.visible').click();
     cy.get('[data-test-id="budgetTags"]').type('Hello');
     cy.get('[data-test-id="primary-button"]').click();
   });
