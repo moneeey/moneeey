2c4e439c7af392aa452d2426856d98f2b0ddc3d3
Fernando Baroni <f@baroni.tech>
2022-10-17 09:18:36 -0300

    fix: tour.cy budget loading bar always visible

--- frontend/src/pages/budget/BudgetPeriod.tsx
+++ frontend/src/pages/budget/BudgetPeriod.tsx
@@ -25,7 +25,7 @@ const BudgetPeriods = observer(
     const [progress, setProgress] = useState(0);
 
     return (
-      <Loading loading={progress !== 100} progress={progress}>
+      <Loading loading={progress !== 0 && progress !== 100} progress={progress}>
         <div className='periods'>
           {map(range(0, viewMonths), (offset) => (
             <BudgetPeriod
