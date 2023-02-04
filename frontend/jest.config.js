8003f63b244a9ed654f5ab97fd8629c60e533ef3
Fernando Baroni <f@baroni.tech>
2023-02-04 08:27:40 -0300

    Jest config js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-}
+};
