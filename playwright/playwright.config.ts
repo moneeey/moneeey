a3c3c77752f5870e6c20060fe3b5a3a49a28d044
Fernando Baroni <f@baroni.tech>
2023-07-23 11:56:21 -0300

    Pass CI flag to run server

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://local.moneeey.io:4280',
+    baseURL: process.env.CI ? 'http://127.0.0.1:4270/' : 'http://local.moneeey.io:4280',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
