df0c8e75a1b6308b56231391742291408dbbf758
Fernando Baroni <f@baroni.tech>
2024-07-21 15:33:31 -0300

    Improve playwright

--- playwright/tests/tour.spec.ts
+++ playwright/tests/tour.spec.ts
@@ -1,28 +1,57 @@
 import { Locator, Page, expect, test } from '@playwright/test';
 
 const mostUsedCurrencies = [
-  "Real brasileiro",
-  "United States dollar",
-  "Euro",
-  "Japonese yen",
-  "British sterling",
-  "Australian dollar",
-  "Canadian dollar",
-  "Swiss franc",
-  "Chinese renminbi",
-  "Hong Kong dollar",
-  "New Zealand dollar",
-  "Swedish krona",
-  "South Korean won",
-  "Singapore dollar",
-  "Norwegian krone",
-  "Mexican peso",
-  "Indian rupee",
-  "Russian ruble",
-  "South African rand",
-  "Turkish lira",
-  "Bitcoin",
-  "Etherium",
+  "BRL Brazilian Real",
+  "USD United States Dollar",
+  "EUR Euro",
+  "BTC Bitcoin",
+  "ETH Ethereum",
+  "JPY Japanese Yen",
+  "GBP British Pound Sterling",
+  "AUD Australian Dollar",
+  "CAD Canadian Dollar",
+  "CHF Swiss Franc",
+  "CNY Chinese Yuan",
+  "SEK Swedish Krona",
+  "NZD New Zealand Dollar",
+  "MXN Mexican Peso",
+  "SGD Singapore Dollar",
+  "HKD Hong Kong Dollar",
+  "NOK Norwegian Krone",
+  "KRW South Korean Won",
+  "TRY Turkish Lira",
+  "RUB Russian Ruble",
+  "INR Indian Rupee",
+  "ZAR South African Rand",
+  "PHP Philippine Peso",
+  "CZK Czech Koruna",
+  "IDR Indonesian Rupiah",
+  "MYR Malaysian Ringgit",
+  "HUF Hungarian Forint",
+  "ISK Icelandic Krona",
+  "HRK Croatian Kuna",
+  "BGN Bulgarian Lev",
+  "ILS Israeli New Shekel",
+  "CLP Chilean Peso",
+  "AED UAE Dirham",
+  "SAR Saudi Riyal",
+  "RON Romanian Leu",
+  "COP Colombian Peso",
+  "THB Thai Baht",
+  "VND Vietnamese Dong",
+  "EGP Egyptian Pound",
+  "PEN Peruvian Sol",
+  "PKR Pakistani Rupee",
+  "KWD Kuwaiti Dinar",
+  "UAH Ukrainian Hryvnia",
+  "BDT Bangladeshi Taka",
+  "ARS Argentine Peso",
+  "DZD Algerian Dinar",
+  "MAD Moroccan Dirham",
+  "JOD Jordanian Dinar",
+  "BHD Bahraini Dinar",
+  "OMR Omani Rial",
+  "RSD Serbian Dinar",
 ]
 
 function Select(page: Page, testId: string) {
@@ -38,6 +67,9 @@ function Select(page: Page, testId: string) {
   }
 
   return {
+    async value() {
+      return select.locator('.mn-select__single-value').innerText()
+    },
     async options() {
       await open()
       return await menuList.locator('.mn-select__option').allTextContents()
@@ -70,18 +102,18 @@ function Input(page: Page, testId: string, container?: Locator) {
 }
 
 async function BudgetEditorSave(page: Page, name: string, currency: string, tag: string) {
-    const budgetEditor = page.getByTestId('budgetEditorDrawer')
-    await Input(page, 'budgetName', budgetEditor).change(name)
+  const budgetEditor = page.getByTestId('budgetEditorDrawer')
+  await Input(page, 'budgetName', budgetEditor).change(name)
 
-    const budgetCurrency = Select(page, 'budgetCurrency')
-    expect(await budgetCurrency.options()).toEqual(mostUsedCurrencies)
-    await budgetCurrency.choose(currency)
+  const budgetCurrency = Select(page, 'budgetCurrency')
+  expect(await budgetCurrency.options()).toEqual(mostUsedCurrencies)
+  await budgetCurrency.choose(currency)
 
-    const budgetTags = Select(page, 'budgetTags')
-    expect(await budgetTags.options()).toEqual(['Account test', 'Gas Station'])
-    await budgetTags.choose(tag, false)
+  const budgetTags = Select(page, 'budgetTags')
+  expect(await budgetTags.options()).toEqual(['Account test', 'Gas Station'])
+  await budgetTags.choose(tag, false)
 
-    await budgetEditor.getByTestId('budgetSave').click()
+  await budgetEditor.getByTestId('budgetSave').click()
 }
 
 async function dismissNotification(page: Page, text: string) {
@@ -96,16 +128,59 @@ function tourNext(page: Page) {
   return page.getByTestId('nm-modal-card').getByTestId('next').click() // Start Tour
 }
 
+async function completeLandingWizard(page: Page) {
+  await expect(page.getByTestId('minimalScreenTitle')).toContainText('Moneeey')
+  // Select language
+  expect(page.getByText('Select language')).toBeDefined()
+  expect(page.getByTestId('languageSelector_portuguese')).toBeDefined()
+  expect(page.getByTestId('languageSelector_spanish')).toBeDefined()
+  expect(page.getByTestId('languageSelector_english')).toBeDefined()
+  await (page.getByTestId('languageSelector_spanish')).click()
+  await expect(page.getByTestId('ok-button')).toContainText('Continúa')
+  await (page.getByTestId('languageSelector_english')).click()
+  await expect(page.getByTestId('ok-button')).toContainText('Continue')
+
+  // Select default currency
+  await (page.getByTestId('ok-button')).click()
+  expect(page.getByTestId('defaultCurrencySelector')).toBeDefined()
+  const defaultCurrencySelector = Select(page, 'defaultCurrencySelector')
+  expect(await defaultCurrencySelector.options()).toEqual(mostUsedCurrencies)
+  await defaultCurrencySelector.choose(mostUsedCurrencies[0])
+  await expect(page.getByTestId('ok-button')).toContainText('Continue')
+  await (page.getByTestId('ok-button')).click()
+
+  // Create two initial accounts
+  await (Input(page, 'name').change('Banco Moneeey'))
+  expect(await (Select(page, 'editorCurrency').value())).toEqual('BRL Brazilian Real')
+  await (Input(page, 'editorInitial_balance').change('1234,56'))
+  await (page.getByTestId('save-and-add-another')).click()
+
+  await (Input(page, 'name').change('Bitcoinss'))
+  await (Select(page, 'editorCurrency').choose('BTC Bitcoin'))
+  await (Input(page, 'editorInitial_balance').change('0,123456789'))
+  expect(await (Select(page, 'editorCurrency').value())).toEqual('BTC Bitcoin')
+  await (page.getByTestId('save-and-close')).click()
+
+  await expect(page.getByText('Dashboard')).toBeVisible()
+}
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
   await page.evaluate(() => {
+    while (window.localStorage.length) {
+      const key = window.localStorage.key(0)
+      if (key) {
+        window.localStorage.removeItem(key)
+      }
+    }
     window.indexedDB.deleteDatabase('_pouch_moneeey');
   });
 });
 
 test.describe('Tour', () => {
   test('Moneeey Tour', async ({ page, }) => {
-    await expect(page.getByTestId('nm-modal-title')).toContainText('Introducing Moneeey')
+    await completeLandingWizard(page)
+
     await page.getByTestId('start-tour').click() // Start Tour
 
     expect(page.getByText('please edit the currencies')).toBeDefined()
@@ -113,12 +188,7 @@ test.describe('Tour', () => {
 
     // Accounts page
     expect(page.getByText('Now that we know the currencies')).toBeDefined()
-
-    // Dismiss error continuing without creating an account
     await tourNext(page) // Next on Edit accounts
-    expect(page.getByText('Now that we know the currencies')).toBeDefined() // Fails because must create account before proceeding
-
-    await dismissNotification(page, 'Before continuing, please create an account by typing its information in the table below.')
 
     // Type account in table to create a new account
     await Input(page, 'editorName').change('Account test')
@@ -176,6 +246,6 @@ test.describe('Tour', () => {
     await tourNext(page)
 
     // Tour is closed
-    // FIXME: expect(page.getByTestId('nm-modal-title')).toBeHidden()
+    expect(page.getByTestId('nm-modal-title')).toBeHidden()
   });
 });
