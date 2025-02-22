28d08d09215cf421c3e3895e6677e7972600b4dc
Fernando Baroni <f@baroni.tech>
2025-02-22 14:45:42 -0300

    Upgrade playwright

--- playwright/README_videos.sh
+++ playwright/README_videos.sh
@@ -5,7 +5,7 @@ yarn test-cr --reporter=null
 function slowdown() {
   local NAME=$1
   local VID_PATH=$2
-  local OUT_VID="../${NAME}.webm"
+  local OUT_VID="./${NAME}.webm"
   rm -f "${OUT_VID}"
   ffmpeg -i "${VID_PATH}" -filter:v "setpts=5.0*PTS" -af "atempo=0.5" -c:v libvpx-vp9 -b:v 2M -c:a libopus "${OUT_VID}"
 
