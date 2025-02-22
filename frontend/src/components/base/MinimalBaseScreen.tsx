bde0032053011ba5a18968c34d06bd8d4c9dfe6e
Fernando Baroni <f@baroni.tech>
2025-02-22 14:30:51 -0300

    Fix initial screen positioning

--- frontend/src/components/base/MinimalBaseScreen.tsx
+++ frontend/src/components/base/MinimalBaseScreen.tsx
@@ -8,7 +8,7 @@ export default function MinimalBasicScreen({
 	const Messages = useMessages();
 	return (
 		<div className="flex justify-center items-center min-h-screen bg-background-600 ">
-			<div className="flex flex-col items-center gap-4 md:scale-150 pb-32">
+			<div className="flex flex-col items-center gap-4 md:scale-150">
 				<h1
 					className="flex flex-row gap-2 scale-150 pb-4"
 					data-testid="minimalScreenTitle"
--- frontend/src/components/base/Modal.tsx
+++ frontend/src/components/base/Modal.tsx
@@ -80,7 +80,7 @@ const Modal = observer(
 		}
 		return (
 			<article
-				className={`fixed top-0 bottom-0 left-0 right-0 z-50 bg-background-600 ${
+				className={`absolute top-0 bottom-0 left-0 right-0 z-50 bg-background-600 ${
 					className || ""
 				}`}
 			>
