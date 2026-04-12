558e4e12467e57d9289742d5b818819fc9d42755
Fernando Baroni <f@baroni.tech>
2026-04-12 11:15:50 -0300

    Fix theme/lang selector outline

--- frontend/src/components/ThemeSwitcher.tsx
+++ frontend/src/components/ThemeSwitcher.tsx
@@ -52,8 +52,8 @@ export default function ThemeSwitcher() {
 		return (
 			<i
 				data-testid={`themeSwitcher_${value}`}
-				className={`inline-block h-6 w-6 rounded-xl hover:ring-3 ring-secondary-500 p-0.5 ${
-					isActive ? "ring-3" : ""
+				className={`inline-block h-6 w-6 rounded-xl hover:ring-4 ring-secondary-500 p-0.5 ${
+					isActive ? "ring-4" : ""
 				}`}
 				onClick={() => selectMode(value)}
 				onKeyDown={() => selectMode(value)}
